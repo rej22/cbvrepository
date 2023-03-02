2902b087f3f913cd1bab4220f4001edc1c09b1d7
rej22 <rejilraj813@outlook.com>
2023-03-02 16:05:08 +0530

    alterations

--- cbv_folder/cbv_project/search_app/urls.py
+++ cbv_folder/cbv_project/search_app/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+from cbv_app.views import MovieListView, MovieDetailView
 
 app_name = 'search_app'
 urlpatterns = [
 
     path('', views.searchResult, name='searchResult'),
+    path('detail/<int:pk>/', MovieDetailView.as_view(), name='detail'),
 ]
--- cbv_folder/cbv_project/search_app/views.py
+++ cbv_folder/cbv_project/search_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
 from cbv_app.models import Movie
 from django.db.models import Q
+
 
 
 def searchResult(request):
@@ -10,3 +13,9 @@ def searchResult(request):
         query = request.GET.get('q')
         searchedMovies = Movie.objects.all().filter(Q(movieName__contains=query)| Q(movieInfo__contains=query))
     return render(request, 'search.html', {'query': query, 'searchedMovies': searchedMovies})
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = 'detail.html'
+    context_object_name = 'movieDetailObj'
