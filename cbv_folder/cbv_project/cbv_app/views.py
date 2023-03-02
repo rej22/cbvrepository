b4f1572613acff3c49b49b6ee6fc089788930bb4
rej22 <rejilraj813@outlook.com>
2023-03-02 09:44:10 +0530

    changes for delete.html page

--- cbv_folder/cbv_project/cbv_app/views.py
+++ cbv_folder/cbv_project/cbv_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic import UpdateView, DeleteView
 from cbv_app.models import Movie
 
 
@@ -44,5 +44,4 @@ class MovieUpdateView(UpdateView):
 class MovieDeleteView(DeleteView):
     model = Movie
     template_name = 'delete.html'
-    context_object_name = 'movieDeleteObj'
-    success_url = reverse_lazy('cbv_app:home')
+    success_url = reverse_lazy('cbv_app:cbvhome')
